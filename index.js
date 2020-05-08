2f59c1e7418bfc662a2407560ec435adc5262a15
Rob Hess <robwhess@gmail.com>
2020-05-08 09:55:11 -0700

    Add code from 5/8 lecture.

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-window.addEventListener('click', function () {
-  console.log("== The window was clicked");
+window.addEventListener('click', function (event) {
+  console.log("== The window was clicked, event.target:", event.target);
 });
 
-function buttonClickListener() {
-  console.log("== A button was clicked");
+function buttonClickListener(event) {
+  console.log("== A button was clicked, event:", event);
+  var button = event.currentTarget;
+  var box = button.parentNode;
+  box.classList.toggle("highlighted");
 }
 
+/*
+ * .in-box-button
+ */
 var buttons = document.getElementsByClassName("in-box-button");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', buttonClickListener);
 }
+
+/*
+ * .box
+ */
+var boxes = document.getElementsByClassName("box");
+for (var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', function (event) {
+    console.log("== A box was clicked");
+    console.log("  -- event.target:", event.target);
+    console.log("  -- event.currentTarget:", event.currentTarget);
+    event.stopPropagation();
+  });
+}
+
+/*
+ * #website-link
+ */
+var link = document.getElementById("website-link");
+link.addEventListener('click', function (event) {
+  console.log("== The link was clicked");
+  event.preventDefault();
+});
+
+/*
+ * #box-container .box.highlighted
+ */
+var highlightedBox = document.querySelector("#box-container .box.highlighted");
--- wordBoxes.js
+++ wordBoxes.js
@@ -2,11 +2,19 @@ var allWords = [];
 var currentWord = 0;
 
 function handleNewWordsEntered(event) {
-  var text = '';
+  var text = event.currentTarget.value;
   allWords = text.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '')
     .toLowerCase().split(' ');
   currentWord = 0;
+  console.log("== Event was triggered, text:", text);
+  console.log("  -- allWords:", allWords);
 }
+
+
+var wordsInputElem = document.getElementById("words-input");
+// wordsInputElem.addEventListener('input', handleNewWordsEntered);
+wordsInputElem.addEventListener('change', handleNewWordsEntered);
+
 
 function generateWordElem(word, highlightColor) {
 	var wordElem = document.createElement('span');
